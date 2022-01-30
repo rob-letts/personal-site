fd7e15fe9904d7bb30fa4efac0b90c2b735144ac
Rob Letts <robertspencerletts@gmail.com>
2022-01-30 19:06:18 +0000

    chore: refactor

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,54 +1,73 @@
 <template>
-  <header>
-    <h1>hi there <img class="emoji" src="/logo-wave.png" alt="a waving hand emoji" /></h1>
-    <p>
-      i’m <span id="name" class="text-lg">Rob Letts</span> - a front-end web developer
-      based in
-      <span class="no-wrap">
-        South London.
-        <span class="emoji"
-          ><img class="emoji" src="/logo-coffee.png" alt="a coffee cup emoji"
-        /></span>
-      </span>
-    </p>
-  </header>
+	<header>
+		<h1>
+			hi there
+			<span
+				><img
+					class="emoji"
+					src="/logo-wave.png"
+					alt="a waving hand emoji"
+			/></span>
+		</h1>
+		<p>
+			i’m <span id="name" class="text-lg">Rob Letts</span> - a front-end
+			web developer based in
+			<span class="no-wrap">
+				South London.
+				<span
+					><img
+						class="emoji"
+						src="/logo-coffee.png"
+						alt="a coffee cup emoji"
+				/></span>
+			</span>
+		</p>
+	</header>
 </template>
 
 <style>
 header {
-  text-align: center;
+	text-align: center;
 }
 
 #name {
-  font-weight: 700;
-  background-image: linear-gradient(to right, var(--primaryColor), var(--secondaryColor));
-  background-clip: text;
-  color: transparent;
-  position: relative;
+	font-weight: 700;
+	background-image: linear-gradient(
+		to right,
+		var(--primaryColor),
+		var(--secondaryColor)
+	);
+	background-clip: text;
+	color: transparent;
+	position: relative;
 }
 
 #name:after {
-  content: '';
-  position: absolute;
-  bottom: -0.5rem;
-  left: 0px;
-  width: 100%;
-  border-style: solid;
-  border-image: linear-gradient(to right, var(--primaryColor), var(--secondaryColor));
-  border-image-slice: 100%;
-  border-image-slice: 1;
-  transform: scaleX(0);
-  transition: transform 0.25s ease-out;
-  transform-origin: bottom left;
+	content: '';
+	position: absolute;
+	bottom: -0.5rem;
+	left: 0px;
+	width: 100%;
+	border-style: solid;
+	border-image: linear-gradient(
+		to right,
+		var(--primaryColor),
+		var(--secondaryColor)
+	);
+	border-image-slice: 100%;
+	border-image-slice: 1;
+	transform: scaleX(0);
+	transition: transform 0.25s ease-out;
+	transform-origin: bottom left;
 }
 
 #name:hover:after {
-  transform: scaleX(1);
+	transform: scaleX(1);
 }
 
 @media only screen and (min-width: 400px) {
-  header {
-    text-align: left;
-  }
+	header {
+		text-align: left;
+	}
 }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,127 +3,153 @@ import { ref } from 'vue';
 import Link from './Link.vue';
 
 let preContent = ref(true);
+
+function toggleContent() {
+	preContent.value = !preContent.value;
+}
 </script>
 
 <template>
-  <main>
-    <nav>
-      <button @click="preContent = true" :disabled="preContent">before 2020</button>
-      <button @click="preContent = false" :disabled="!preContent">since 2020</button>
-    </nav>
+	<main>
+		<nav>
+			<button @click="toggleContent" :disabled="preContent">
+				before 2020
+			</button>
+			<button @click="toggleContent" :disabled="!preContent">
+				since 2020
+			</button>
+		</nav>
+		<template v-if="preContent">
+			<ul>
+				<li>
+					ran a music venue called
+					<Link href="https://www.youtube.com/watch?v=cwPCiHk081k"
+						>Corsica Studios</Link
+					>
+					for 8 years.
+				</li>
+				<li>
+					made noisy metal music in
+					<Link
+						href="https://disciplesofwren.bandcamp.com/album/groundswells"
+						>Wren</Link
+					>
+					for 6 years.
+				</li>
 
-    <ul>
-      <template v-if="preContent">
-        <li>
-          ran a music venue called
-          <Link href="https://www.youtube.com/watch?v=cwPCiHk081k">Corsica Studios</Link>
-          for 8 years.
-        </li>
-
-        <li>
-          made noisy metal music in
-          <Link href="https://disciplesofwren.bandcamp.com/album/groundswells">Wren</Link>
-          for 6 years.
-        </li>
-        <li>
-          got a BA in sound design and barely used it.
-          <span
-            ><img
-              class="emoji"
-              id="thumbs-down"
-              src="/public/logo-thumbs-down.png"
-              alt="a thumbs down emoji"
-          /></span>
-        </li>
-      </template>
-      <template v-else>
-        <li>
-          somehow got a distinction grade <strong>MSc</strong> in Computer Science.
-          <img
-            class="emoji"
-            src="logo-explode.png"
-            alt="an person's exploding head emoji"
-          />
-        </li>
-        <li>
-          started as a junior front end dev at
-          <Link href="https://www.redsnapper.net/">Red Snapper</Link>, primarily making
-          static sites for the British Medical Journal.
-        </li>
-        <li>
-          became deeply fascinated with the
-          <strong>Vue.js</strong> and <strong>CSS</strong> ecosystems.
-        </li>
-      </template>
-    </ul>
-  </main>
+				<li>
+					got a BA in sound design and barely used it.
+					<span
+						><img
+							class="emoji"
+							id="thumbs-down"
+							src="/public/logo-thumbs-down.png"
+							alt="a thumbs down emoji"
+					/></span>
+				</li>
+			</ul>
+		</template>
+		<template v-else>
+			<ul>
+				<li>
+					somehow got a distinction grade <strong>MSc</strong> in
+					Computer Science.
+					<img
+						class="emoji"
+						src="logo-explode.png"
+						alt="an person's exploding head emoji"
+					/>
+				</li>
+				<li>
+					started as a junior front end dev at
+					<Link href="https://www.redsnapper.net/">Red Snapper</Link>,
+					implementing designs primarily for the British Medical
+					Journal.
+				</li>
+				<li>
+					became deeply fascinated with the
+					<strong>Vue.js</strong> and <strong>CSS</strong> ecosystems.
+				</li>
+			</ul>
+		</template>
+	</main>
 </template>
 
 <style scoped>
 main {
-  margin-bottom: 5rem;
-  text-align: center;
+	margin-bottom: 5rem;
+	text-align: center;
 }
 
 h2 {
-  font-size: 1.2em;
-  margin-bottom: 0.5rem;
+	font-size: 1.2em;
+	margin-bottom: 0.5rem;
 }
 
 nav {
-  display: flex;
-  gap: 1rem;
-  margin-bottom: 1.5rem;
-  justify-content: center;
+	display: flex;
+	gap: 1rem;
+	margin-bottom: 1.5rem;
+	justify-content: center;
 }
 
 button {
-  padding: 0.25rem 1rem;
-  background: transparent;
-  width: clamp(150px, 200px, 25%);
-  color: var(--textColor);
-  border: 1px solid var(--textColor);
-  cursor: pointer;
-  border-radius: 7px;
-  transition: background 0.25s ease-out;
+	padding: 0.25rem 1rem;
+	background: transparent;
+	width: clamp(150px, 200px, 25%);
+	color: var(--textColor);
+	border: 1px solid var(--textColor);
+	cursor: pointer;
+	border-radius: 7px;
+	transition: background 0.25s ease-out;
 }
 
 strong {
-  font-weight: 700;
-  color: var(--primaryColor);
+	font-weight: 700;
+	color: var(--primaryColor);
 }
 
 li {
-  margin-bottom: 1rem;
-  list-style-type: '+';
-  list-style-position: inside;
+	margin-bottom: 1rem;
+	list-style-type: '+';
+	list-style-position: inside;
 }
 
 #thumbs-down {
-  vertical-align: bottom;
-  margin-left: 0.5rem;
+	vertical-align: bottom;
+	margin-left: 0.5rem;
 }
 
 button:disabled {
-  color: var(--backgroundColor);
-  background: var(--textColor);
+	color: var(--backgroundColor);
+	background: var(--textColor);
 }
 
 button:not(:disabled):hover {
-  transform: scale(1.025);
+	transform: scale(1.025);
 }
 
 button:not(:disabled):active {
-  transform: scale(1.075);
+	transform: scale(1.075);
 }
 
 @media only screen and (min-width: 400px) {
-  nav {
-    justify-content: flex-start;
-  }
+	nav {
+		justify-content: flex-start;
+	}
 
-  main {
-    text-align: left;
-  }
+	main {
+		text-align: left;
+	}
+}
+
+.v-enter-active,
+.v-leave-active {
+	transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+	opacity: 0;
 }
 </style>
