fb35033ce4262a288f9b2be61556f4666703d5b1
Rob Letts <robertspencerletts@gmail.com>
2022-02-08 07:14:21 +0000

    chore: install and config postcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import postcssNesting from 'postcss-nesting';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
 	plugins: [vue()],
 	resolve: {
 		alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
 	},
+	css: { postcss: { plugins: [postcssNesting, autoprefixer] } },
 });
