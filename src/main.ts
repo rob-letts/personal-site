6cd0ad94c98a925b1d6e8d39ad512e0c7c706514
Rob Letts <robertspencerletts@gmail.com>
2022-04-15 12:12:07 +0100

    chore: remove postcss

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import { createApp } from 'vue';
-import { createPinia } from 'pinia'
 import App from './App.vue';
 
-
-createApp(App).use(createPinia()).mount('#app');
+createApp(App).mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
-import { fileURLToPath, URL } from 'url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import postcssNesting from 'postcss-nesting';
-import autoprefixer from 'autoprefixer';
+import { fileURLToPath, URL } from "url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
 	plugins: [vue()],
-	resolve: {
-		alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
-	},
-	css: { postcss: { plugins: [postcssNesting, autoprefixer] } },
+	resolve: {alias: { "@": fileURLToPath(new URL("./src", import.meta.url)) },},
 });
