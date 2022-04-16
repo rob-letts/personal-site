2f5ee1f1ac538121cdfac341f84c7d40c2802b6e
Rob Letts <robertspencerletts@gmail.com>
2022-04-16 10:55:54 +0100

    feat: add recall users previous theme setting

--- src/components/ThemeToggle.vue
+++ src/components/ThemeToggle.vue
@@ -4,13 +4,22 @@ import { MoonSVG, SunSVG } from "../svg";
 
 const darkThemeActive = ref<null | boolean>(null);
 onMounted(() => {
-	darkThemeActive.value = window?.matchMedia('(prefers-color-scheme: dark)').matches;
+	// Check local storage
+	const previousSetting = localStorage.getItem("darkThemeActive");
+	if (previousSetting) {
+		darkThemeActive.value = JSON.parse(previousSetting);
+		document.documentElement.classList.remove(darkThemeActive.value ? "lightTheme" : "darkTheme");
+		document.documentElement.classList.add(darkThemeActive.value ? "darkTheme" : "lightTheme");
+	} else {
+		darkThemeActive.value = window?.matchMedia('(prefers-color-scheme: dark)').matches;
+	}
 })
 
 function toggleTheme() {
 	darkThemeActive.value = !darkThemeActive.value;
 	document.documentElement.classList.remove(darkThemeActive.value ? "lightTheme" : "darkTheme");
 	document.documentElement.classList.add(darkThemeActive.value ? "darkTheme" : "lightTheme");
+	localStorage.setItem("darkThemeActive", JSON.stringify(darkThemeActive.value));
 }
 </script>
 
