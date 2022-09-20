30c8c270c8baae1deff78774add94e71c5ea0622
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2022-09-20 08:56:43 +0100

    remove token css

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import presetEnv from 'postcss-preset-env';
 import autoprefixer from 'autoprefixer';
-import tokencss from "@tokencss/astro";
 
 export default defineConfig({
-  integrations: [vue(), tokencss()],
+  integrations: [vue()],
   vite: {
     css: {
       postcss: {
--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import presetEnv from 'postcss-preset-env';
-import autoprefixer from 'autoprefixer';
-
-export default defineConfig({
-  server: { port: 3000 },
-  css: { postcss: { plugins: [presetEnv({ stage: 1 }), autoprefixer] }}
-});
