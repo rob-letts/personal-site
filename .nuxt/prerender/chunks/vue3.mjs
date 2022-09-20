b3f6771ac92c005ae03047cf9937dd70f385fad1
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2022-09-20 08:57:30 +0100

    remove nuxt site

--- .nuxt/prerender/chunks/vue3.mjs
+++
@@ -1,9 +0,0 @@
-import { s as serverRenderer } from './index.mjs';
-import 'file:///Users/robletts/Dev/projects/personal-site/node_modules/unenv/runtime/mock/proxy.mjs';
-import 'stream';
-
-const renderToString = (...args) => {
-  return serverRenderer.exports.renderToString(...args).then((result) => `<div id="__nuxt">${result}</div>`);
-};
-
-export { renderToString };
