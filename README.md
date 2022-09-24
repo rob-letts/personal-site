95d79b519c770829edb035022ecd58913b9f295d
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2022-09-24 12:01:53 +0100

    reverted site back to vanilla implementation

--- README.md
+++ README.md
@@ -1,21 +1,3 @@
 👋 [Available here](https://roblettsdev.netlify.app/)
 
-A super minimal 'contact me' static site on Netlify built with Astro, TypeScript, and PostCSS.
-
-Includes light/dark theme functionality and a responsive layout with fluid typography.
-
-#### Roadmap
-
-- Add TypeScript functionality
-
-- Fix dimensions of SVGs
-
-- alt text not gigantic!
-
-- SVG Outlines Consistent
-
-- Update CV
-
-- Add Subtle bubble Animations
-
-- Add Cascade layers and preset env features
+A super minimal 'contact me' static site on Netlify built with Vite, PostCSS, and TypeScript.
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import presetEnv from 'postcss-preset-env';
 import autoprefixer from 'autoprefixer';
 import { resolve } from 'path';
 
 export default defineConfig({
   server: { port: 3000 },
-  css: { postcss: { plugins: [presetEnv({ stage: 1 }), autoprefixer] }},
-  resolve: { alias: { "@": resolve(__dirname, `src`) }},
-  test: { includeSource: [`src/**/*.{js,ts}`] },
-  define: { 'import.meta.vitest': `undefined` }
+  css: {
+    postcss: {
+      plugins: [
+        presetEnv({ stage: 1 }),
+        autoprefixer
+      ]
+    }
+  },
+  resolve: { alias: { "@": resolve(__dirname, `src`) }}
 });
