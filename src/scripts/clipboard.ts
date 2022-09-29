85052358bf05fb1820ce74401aa011b38202f29a
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2022-09-29 22:45:12 +0100

    extracted typescript concerns to their own modules

---
+++ src/scripts/clipboard.ts
@@ -0,0 +1,15 @@
+const CONTACT_EMAIL = `roblettsdev@gmail.com`;
+const toast = document.querySelector(`.toast`) as HTMLButtonElement;
+
+document.querySelector(`#contact-btn`)?.addEventListener(`click`, () => {
+  copyTextToClipboard(CONTACT_EMAIL);
+
+  if (toast && !toast.classList.contains(`pop-toast`)) {
+    toast.classList.add(`pop-toast`);
+    setTimeout(() => toast.classList.remove(`pop-toast`), 1500);
+  }
+});
+
+function copyTextToClipboard (text: string) {
+  navigator.clipboard.writeText(text);
+}
