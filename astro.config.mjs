5c0e3a4c6e39c0c09499c5a72af00d629a28c2b9
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2023-03-11 23:15:54 +0000

    migraiton: init netlify edge functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
-import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/edge-functions';
 
-// https://astro.build/config
 export default defineConfig({
   output: `server`,
   adapter: netlify()
