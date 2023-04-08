fdfea29545931188423a19b8caf47b58bbb4a872
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2023-04-08 08:15:43 +0100

    refactored theme logic

---
+++ src/scripts/theme.ts
@@ -0,0 +1,20 @@
+export function getTheme(): string | undefined {
+  return document.cookie
+    .split(" ")
+    .find((item) => item.startsWith("theme"))
+    ?.split("=")
+    .at(-1);
+}
+
+export function saveTheme(shouldSetDarkTheme: boolean) {
+  document.cookie = `theme=${
+    shouldSetDarkTheme ? `dark-mode` : `light-mode`
+  }; SameSite=None; Secure`;
+}
+
+export function updateTheme(shouldSetDarkTheme: boolean) {
+  document.documentElement.setAttribute(
+    `class`,
+    shouldSetDarkTheme ? "dark-mode" : "light-mode"
+  );
+}
